18f91474e1528e262c980f09df6a3d18aacde50d
TassiaBarbosa <tassiabarbosa@souunisuam.com.br>
2024-10-16 22:23:01 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ionic.imc_calculadora',
+  appName: 'imc-calculadora',
+  webDir: 'www'
+};
+
+export default config;
