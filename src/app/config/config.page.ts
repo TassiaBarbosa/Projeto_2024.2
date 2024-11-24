70f1cf0996ae562544fdc3c811d544314848f7b9
TassiaBarbosa <tassiabarbosa@souunisuam.com.br>
2024-11-24 17:00:41 -0300

    Update config.page.ts

--- src/app/config/config.page.ts
+++ src/app/config/config.page.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '../services/config.service';
 
 @Component({
   selector: 'app-config',
@@ -7,28 +9,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigPage implements OnInit {
 
-  constructor() { }
+  constructor(private configService: ConfigService) { }
 
   ngOnInit() {
+    const config = this.configService.getConfig();
+    if (config) {
+      this.currentFont = config.font || 'font-default';
+      this.currentFontSizeClass = config.fontSize || 'size-default';
+      this.currentContrastClass = config.contrast || 'contrast-default';
+    }
   }
-
-  /*método para mudar a fonte */
+  
+  //método para mudar a fonte 
   public currentFont: string = 'font-default';
   changeFont(fontClass: string) {
     this.currentFont = fontClass;
   }
 
-  /*método para mudar o tamanho da fonte */
-  public currentFontSizeClass: string = 'font-medium';  
+  //método para mudar o tamanho da fonte
+  public currentFontSizeClass: string = 'size-default';  
   changeFontSize(fontSizeClass: string) { 
     this.currentFontSizeClass = fontSizeClass; 
   }
 
-  /*método para mudar o contraste da fonte */
-  public currentContrastClass: string = 'contrast-medium'; 
+  //método para mudar o contraste da fonte
+  public currentContrastClass: string = 'contrast-default'; 
   changeContrast(contrastClass: string) { 
     this.currentContrastClass = contrastClass; 
   }
+  //método de salvar configurações
+  salvarConfig() {
+    this.configService.setConfig(
+      this.currentFont, this.currentFontSizeClass, this.currentContrastClass
+    ) 
+  }
+
 }
+
+
+
+
+
+
+
+
+
+/**
+import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '../services/config.service';
+
+@Component({
+  selector: 'app-config',
+  templateUrl: './config.page.html',
+  styleUrls: ['./config.page.scss'],
+})
+export class ConfigPage implements OnInit {
+
+  constructor(private configService: ConfigService) { }
+
+  ngOnInit() {
+  }
+  
+  //método para mudar a fonte 
+  public currentFont: string = 'font-default';
+  changeFont(fontClass: string) {
+    this.currentFont = fontClass;
+  }
+
+  //método para mudar o tamanho da fonte
+  public currentFontSizeClass: string = 'size-default';  
+  changeFontSize(fontSizeClass: string) { 
+    this.currentFontSizeClass = fontSizeClass; 
+  }
+
+  //método para mudar o contraste da fonte
+  public currentContrastClass: string = 'contrast-defaut'; 
+  changeContrast(contrastClass: string) { 
+    this.currentContrastClass = contrastClass; 
+  }
+  //método de salvar configurações
+  salvarConfig() {
+    this.configService.setConfig(
+      this.currentFont, this.currentFontSizeClass, this.currentContrastClass
+    ) 
+  }
+
+}
+ */
 
 
